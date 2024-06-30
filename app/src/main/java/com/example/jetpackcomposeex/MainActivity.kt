6c99357856d1ac1600d48068888b729a420075a1
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2024-06-30 22:12:57 +0900

    3 - Build your first app with the best structure - Jetpack Compose

--- app/src/main/java/com/example/jetpackcomposeex/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeex/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.jetpackcomposeex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,37 +11,44 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposeex.ui.theme.JetpackComposeExTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeExTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            FirstApp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun FirstApp() {
+
+    Surface(modifier = Modifier.fillMaxSize()) {
+
+        Column(modifier = Modifier.fillMaxSize()) {
+            Text(text = "Subscribe please", fontSize =  32.sp)
+
+            Test1()
+            Test2()
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    JetpackComposeExTheme {
-        Greeting("Android")
-    }
+fun Test1() {
+    Text(text = "Test 1", fontSize =  32.sp)
+}
+
+@Composable
+fun Test2() {
+    Text(text = "Test 2", fontSize =  32.sp)
+}
+
+@Preview(showBackground = true, name = "Kyo Yamamoto")
+@Composable
+fun AppPreview() {
+    FirstApp()
 }
